fd6d8cceb1480824f3e8ea5dc04271a275704bfd
Ju-yzp <wjpzy20230551@qq.com>
2025-08-29 16:37:52 +0800

    fix: resolve clang-format config error & align with cpplint style

--- cpp/pointer/pointer.cpp
+++ cpp/pointer/pointer.cpp
@@ -4,23 +4,14 @@
 #include <cstddef>
 #include <iostream>
 
-// 修复1：class后大括号紧跟末尾，不单独换行
-class Image
-{
-public:
-  std::size_t rows_{0};
-  std::size_t cols_{0};
-  double * data_{nullptr};
-};
+class Image {};
 
-// 修复3：main()后大括号紧跟末尾，不单独换行
-int main()
-{
-  double obstacles_num = 30;
-  double * ptr = &obstacles_num;
-  Image origin_img;
-  std::cout << sizeof(ptr) << std::endl;
-  Image * img = &origin_img;
-  std::cout << sizeof(img) << std::endl;
-  return 0;
+int main() {
+    double obstacles_num = 30;
+    double* ptr = &obstacles_num;
+    Image origin_img;
+    std::cout << sizeof(ptr) << std::endl;
+    Image* img = &origin_img;
+    std::cout << sizeof(img) << std::endl;
+    return 0;
 }
